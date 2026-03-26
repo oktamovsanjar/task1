1ed7f1f0b5169d2470316ff312b956d3b9565e2c
sanjar o'ktamov <https://github.com/oktamovsanjar>
2026-03-26 02:09:55 +0000

    read

---
+++ app/server.py
@@ -0,0 +1,11 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+class MyHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(b"Hello DevOps")
+
+server = HTTPServer(("0.0.0.0", 8000), MyHandler)
+server.serve_forever()
